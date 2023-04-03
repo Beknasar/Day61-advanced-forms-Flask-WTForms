4135578c16a0b5954f5285aac912be961fd9f6e7
Beknasar <680633@gmail.com>
2023-04-04 01:50:00 +0300

    Added validations to the forms.

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, Email
 
 app = Flask(__name__)
 app.secret_key = "asuhfsaa-123x2w13fd-12xi3u21890-21we2qwex123"
 
 
 class LoginForm(FlaskForm):
-    email = StringField(label="Email: ")
-    password = PasswordField(label="Password: ")
+    email = StringField(label="Email: ", validators=[DataRequired(), Email(check_deliverability=True)])
+    password = PasswordField(label="Password: ", validators=[DataRequired(), Length(min=8)])
     submit = SubmitField(label="Log In")
 
 
@@ -17,10 +18,10 @@ def home():
     return render_template('index.html')
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
-
+    form.validate_on_submit()
     return render_template("login.html", form=form)
 
 
